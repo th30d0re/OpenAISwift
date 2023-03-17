eb611cf4dcb579c792f6224eb53123eb32feb5a9
th30d0re <99168111+th30d0re@users.noreply.github.com>
2023-03-17 18:20:05 -0400

    adding GPT4 & GPT3.5 models
    moved "text-davinci-003" to GPT3_5 and added back OG "davinci" model for clarity and conciseness

--- Sources/OpenAISwift/Models/OpenAIModelType.swift
+++ Sources/OpenAISwift/Models/OpenAIModelType.swift
@@ -24,6 +24,7 @@ public enum OpenAIModelType {
     public var modelName: String {
         switch self {
         case .gpt3(let model): return model.rawValue
+        case .GPT3_5(let model): return model.rawValue
         case .codex(let model): return model.rawValue
         case .feature(let model): return model.rawValue
         case .chat(let model): return model.rawValue
@@ -37,8 +38,8 @@ public enum OpenAIModelType {
         
         /// Most capable GPT-3 model. Can do any task the other models can do, often with higher quality, longer output and better instruction-following. Also supports inserting completions within text.
         ///
-        /// > Model Name: text-davinci-003
-        case davinci = "text-davinci-003"
+        /// > Model Name: davinci
+        case davinci = "davinci"
         
         /// Very capable, but faster and lower cost than GPT3 ``davinci``.
         ///
@@ -54,6 +55,24 @@ public enum OpenAIModelType {
         ///
         /// > Model Name: text-ada-001
         case ada = "text-ada-001"
+    }
+    
+    /// A set of models that can understand and generate natural language
+    ///
+    /// [GPT-3.5 Models OpenAI API Docs](https://beta.openai.com/docs/models/gpt-3.5)
+    public enum GPT3_5: String {
+        
+        /// Can do any language task with better quality, longer output, and consistent instruction-following than the curie, babbage, or ada models. Also supports inserting completions within text.
+        /// Max Tokens: 4,097 tokens
+        /// Training Data: Up to Jun 2021
+        /// > Model Name: text-davinci-003
+        case davinci = "text-davinci-003"
+        
+        /// Similar capabilities to text-davinci-003 but trained with supervised fine-tuning instead of reinforcement learning
+        /// Max Tokens: 4,097 tokens
+        /// Training Data: Up to Jun 2021
+        /// > Model Name: text-davinci-002
+        case davinci002 = "text-davinci-002"
     }
     
     /// A set of models that can understand and generate code, including translating natural language to code
@@ -96,5 +115,16 @@ public enum OpenAIModelType {
         /// Snapshot of gpt-3.5-turbo from March 1st 2023. Unlike gpt-3.5-turbo, this model will not receive updates, and will only be supported for a three month period ending on June 1st 2023.
         /// > Model Name: gpt-3.5-turbo-0301
         case chatgpt0301 = "gpt-3.5-turbo-0301"
+        
+        /// More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat. Will be updated with our latest model iteration.
+        /// Max Tokens: 8,192 tokens
+        /// Training Data: Up to Sep 2021
+        case chatgpt4 = "gpt-4"
+        
+        /// Same capabilities as the base gpt-4 mode but with 4x the context length. Will be updated with our latest model iteration.
+        /// Max Tokens: 32,768 tokens
+        /// Training Data: Up to Sep 2021
+        case chatgpt432k = "gpt-4-32k"
+        
     }
 }
